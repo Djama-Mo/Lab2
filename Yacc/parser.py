08c3117bac9608bb4dfac7acb1fb8ec2246c570e
Djama <59183324+Djama-Mo@users.noreply.github.com>
2020-09-02 13:52:19 +0300

    last updt

--- Yacc/parser.py
+++ Yacc/parser.py
@@ -33,9 +33,15 @@ class Parser:
         """program : stat_list"""
         p[0] = Tree('program', children=p[1], lineno=p.lineno(1))
 
-    # def p_stat_list(self, p):
-    #     """stat_list : NL"""
-    #     p[0] = p[1]
+    def p_stat_group(self, p):
+        """stat_group : L_FIGBRACKET NL stat_list R_FIGBRACKET NL
+                    | statement"""
+        if p[1] == '{':
+            p[1] = Tree('border', value=p[1], lineno=p.lineno(1))
+            p[4] = Tree('border', value=p[4], lineno=p.lineno(1))
+            p[0] = Tree('group_stat', children=[p[1], p[3], p[4]], lineno=p.lineno(2) + 1)
+        else:
+            p[0] = p[1]
 
     def p_stat_list(self, p):
         """stat_list : stat_list statement
@@ -49,21 +55,13 @@ class Parser:
             else:
                 p[0] = Tree('NL', lineno=p.lineno(1))
 
-    def p_stat_group(self, p):
-        """stat_group : L_FIGBRACKET stat_list R_FIGBRACKET NL
-                    | stat_list"""
-        if p[1] == 'L_FIGBRACKET':
-            p[1] = Tree('border', value=p[1], lineno=p.lineno(1))
-            p[4] = Tree('border', value=p[3], lineno=p.lineno(1))
-            p[0] = Tree('group_stat', children=[p[1], p[2], p[3]], lineno=p.lineno(2) + 1)
-        else:
-            p[0] = p[1]
-
     def p_statement(self, p):
         """statement : declaration ENDSTR NL
                     | assignment ENDSTR NL
                     | size ENDSTR NL
                     | to ENDSTR NL
+                    | resize ENDSTR NL
+                    | comp_br ENDSTR NL
                     | for
                     | check
                     | routing
@@ -76,27 +74,24 @@ class Parser:
             p[0] = Tree('EOS', lineno=p.lineno(1))
 
     def p_declaration(self, p):
-        """declaration : type var_list"""
-        if len(p) == 3:
-            p[0] = Tree('declaration', children=[p[1], p[2]], lineno=p.lineno(1))
-        # else:
-        #     p[0] = Tree('declaration', p[2], children=[p[1], p[2], p[4]], lineno=p.lineno(2))
+        """declaration : type var_list EQQ expr"""
+        if len(p) == 5:
+            p[0] = Tree('declaration', children=[p[1], Tree('assign', children=[p[2], p[4]])], lineno=p.lineno(1))
 
     def p_var_list(self, p):
         """var_list : variable
-                    | assignment
-                    | var_list COMMA var_list """
+                    | var_list COMMA var_list"""
         if len(p) == 2:
             p[0] = p[1]
         else:
             p[0] = Tree('var_list', children=[p[1], p[3]], lineno=p.lineno(2))
 
     def p_assignment(self, p):
-        """assignment : variable EQ expr"""
+        """assignment : variable EQQ expr"""
         p[0] = Tree('assignment', value=p[2], children=[p[1], p[3]], lineno=p.lineno(2))
 
     def p_ass_error(self, p):
-        """assignment : variable EQ error"""
+        """assignment : variable EQQ error"""
         p[0] = Tree('error', value='Wrong assignment', children=p[1], lineno=p.lineno(2))
         sys.stderr.write(f'<<<<<Wrong assignment\n')
 
@@ -107,9 +102,9 @@ class Parser:
 
     def p_digit(self, p):
         """digit : INT_DEC
-                |   INT_HEX
-                |   INT_OCT"""
-        p[0] = Tree('digit', value=p[1], lineno=p.lineno(1))
+                | INT_HEX
+                | INT_OCT"""
+        p[0] = Tree(f'digit', value=p[1], lineno=p.lineno(1))
 
     def p_bool(self, p):
         """logic : TRUE
@@ -120,20 +115,16 @@ class Parser:
         """expr : variable
                 | digit
                 | logic
-                | math_expr"""
+                | math_expr
+                | perform
+                | comp"""
         p[0] = p[1]
-        # p[0] = node('expression', ch=p[1], no=p.lineno(1))
 
     def p_math_expr(self, p):
         """math_expr : expr PLUS expr
                     | expr MINUS expr
                     | expr MUL expr
                     | expr DIV expr
-                    | expr EQ expr
-                    | expr LT expr
-                    | expr GT expr
-                    | expr LTE expr
-                    | expr GTE expr
                     | expr AND expr
                     | DENY expr
                     | MOST expr"""
@@ -142,17 +133,28 @@ class Parser:
         else:
             p[0] = Tree('binary_op', value=p[2], children=[p[1], p[3]], lineno=p.lineno(2))
 
-    # def p_mexp_error(self, p):
-    #     """math_expr : expr SMALLER expr
-    #                 | expr LARGER expr"""
-    #     p[0] = Tree('error', value='Comparison error', children=[p[1], p[3]], lineno=p.lineno(2))
-    #     sys.stderr.write(f'<<<<<Wrong comparison\n')
-
     def p_expr_br(self, p):
         """expr : L_SQBRACKET expr R_SQBRACKET"""
         p[1] = Tree('bracket', value=p[1], lineno=p.lineno(1))
         p[3] = Tree('bracket', value=p[3], lineno=p.lineno(3))
         p[0] = Tree('expression', children=[p[1], p[2], p[3]], lineno=p.lineno(1))
+
+    def p_comp(self, p):
+        """comp : EQ expr
+                | LT expr
+                | GT expr
+                | LTE expr
+                | GTE expr"""
+        p[0] = Tree('comparison', value=p[1], children=p[2], lineno=p.lineno(1))
+
+    def p_comp_br(self, p):
+        """comp_br : EQ L_SQBRACKET R_SQBRACKET expr
+                   | LT L_SQBRACKET R_SQBRACKET expr
+                   | GT L_SQBRACKET R_SQBRACKET expr
+                   | LTE L_SQBRACKET R_SQBRACKET expr
+                   | GTE L_SQBRACKET R_SQBRACKET expr"""
+
+        p[0] = Tree('comparison', value=p[1], children=p[4], lineno=p.lineno(1))
 
     def p_variable(self, p):
         """variable : VARIABLE
@@ -164,46 +166,48 @@ class Parser:
 
     def p_arr_var(self, p):
         """arr_var : VARIABLE indexing
-                    | VARIABLE"""
-        p[0] = Tree('array', p[1], lineno=p.lineno(2))
+                   | arr_var COMMA arr_var"""
+        if p[2] == ',':
+            p[0] = Tree('array_comma', children=[p[1], p[3]], lineno=p.lineno(2))
+        else:
+            p[0] = Tree('array_lvl', value=p[1], children=p[2], lineno=p.lineno(2))
 
     def p_indexing(self, p):
         """indexing : L_SQBRACKET INT_DEC R_SQBRACKET
                     | L_SQBRACKET INT_DEC R_SQBRACKET indexing"""
         if len(p) == 4:
             p[0] = Tree('indexing', children=p[2], lineno=p.lineno(1))
-        else:
-            p[0] = Tree('index', children=[p[2], p[4]], lineno=p.lineno(1))
+        elif len(p) == 3:
+            p[0] = Tree('indexing', children=[], lineno=p.lineno(1))
 
     def p_size(self, p):
-        """size : SIZE LBRACKET VARIABLE RBRACKET"""
-        p[0] = Tree('size', value=p[3], children=p[3], lineno=p.lineno(1), lexpos=p.lexpos(1))
+        """size : SIZE LBRACKET VARIABLE RBRACKET
+                | SIZE LBRACKET type RBRACKET"""
+        p[0] = Tree('size', value=p[3], lineno=p.lineno(1), lexpos=p.lexpos(1))
 
     def p_to(self, p):
         """to : TO type VARIABLE"""
-        p[0] = Tree('type change', value=[p[2], p[3]], children=p[3], lineno=p.lineno(1))
+        p[0] = Tree('type change', value=[p[3], p[2]], children=p[3], lineno=p.lineno(1))
 
     def p_resize(self, p):
         """resize : RESIZE VARIABLE L_SQBRACKET INT_DEC R_SQBRACKET"""
-        p[0] = Tree('resize', value=p[1], children=p[2], lineno=p.lineno(1))
+        p[0] = Tree('resize', value=[p[2], p[4]], children=p[2], lineno=p.lineno(1))
 
     def p_perform(self, p):
         """perform : PERFORM VARIABLE var_list"""
-        p[0] = Tree('perform', value=p[1], children=p[2], lineno=p.lineno(1))
+        p[0] = Tree('perform', value=p[2], children=p[3], lineno=p.lineno(1))
 
     def p_for(self, p):
-        """for : FOR VARIABLE STOP VARIABLE STEP VARIABLE NL stat_group ENDSTR NL"""
-        p[0] = Tree('for', children={'start': Tree('variable', p[2], children=[]),
-                                     'stop': p[3],
-                                     'finish': Tree('variable', p[4], children=[]),
-                                     'step': p[5],
-                                     'pace': Tree('variable', p[4], children=[]),
+        """for : FOR VARIABLE STOP VARIABLE STEP VARIABLE NL stat_group"""
+        p[0] = Tree('for', children={'start': Tree('variable', p[2]),
+                                     'step': Tree('variable', p[6]),
+                                     'stop': Tree('variable', p[4]),
                                      'body': p[8]}, lineno=p.lineno(1))
 
     def p_check(self, p):
-        """check : CHECK expr THEN NL stat_group ENDSTR NL
-                | CHECK expr THEN NL stat_group OTHERWISE NL stat_group ENDSTR NL"""
-        if len(p) == 7:
+        """check : CHECK expr THEN NL stat_group
+                | CHECK expr THEN NL stat_group OTHERWISE NL stat_group"""
+        if len(p) == 6:
             p[0] = Tree('if_then', children={'condition': p[2], 'body': p[5]}, lineno=p.lineno(1))
         else:
             p[0] = Tree('if_then', children={'condition': p[2], 'body_1': p[5], 'body_2': p[8]}, lineno=p.lineno(1))
@@ -219,7 +223,8 @@ class Parser:
             sys.stderr.write(f'<<<<<Redeclared function\n')
         else:
             p[0] = Tree('function', value=str(p[3]), children={'parameters': p[4],
-                                                               'body': p[6]}, lineno=p.lineno(1))
+                                                               'body': p[6],
+                                                               'return': p[8]}, lineno=p.lineno(1))
             self.functions[p[3]] = p[0]
 
     def p_routing_error(self, p):
